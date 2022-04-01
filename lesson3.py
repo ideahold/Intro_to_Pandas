aa48ad7cc75d2766c7b2cfde74f7162d56e42ab2
Vyacheslav Pugovkin <72509800+ideahold@users.noreply.github.com>
2022-04-01 16:38:09 +0300

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -3,19 +3,21 @@ data = pd.read_csv('data.csv', index_col=0)
 
 data.describe()
 
-data.points.describe()
+data.points.describe() # описание столбца (или всей таблицы), подробнее можно узнать в видео на канале
 
-data.points.mean()
+data.points.mean() # выводим среднее значение по столбцу
 
-data.taster_name.unique()
+data.taster_name.unique() # выводим все значения без повторений
 
-data.taster_name.value_counts()
+data.taster_name.value_counts() # количество каждого значения в столбце
 
 
 data_mean_points = data.points.mean()
-data.points.map(lambda p: p - data_mean_points)
+# map позволяет применить некоторые действия ко всему столбцу
+# lambda принимает (p) и возвращает (p - data_mean_points) для каждой ячейки
+data.points.map(lambda p: p - data_mean_points) 
 
-
+# аналогичная запись с использованием apply()
 def rp(row):
     row.points = row.points - data_mean_points
     return row
